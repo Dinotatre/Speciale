0e43d262d56fb7dac5ca33074778947ab059611e
dinotatre <simonesbjergchristensen@gmail.com>
2025-10-21 11:56:48 +0200

    Still testing phase

--- main.R
+++ main.R
@@ -5,7 +5,7 @@
 source("functions.R")
 
 # Example usage of a function from functions.R
-result <- greet("World")
+result <- greet("Freja")
 print(result)
 
 # --- Main analysis workflow starts here ---
@@ -14,3 +14,11 @@ print(result)
 # data <- read.csv("your_data.csv")
 # analysis_result <- your_analysis_function(data)
 # print(analysis_result)
+
+#Intialize values:
+
+S0<-1
+A0<-1
+r<-0.02
+mu<-0.07
+sigma<-0.2
